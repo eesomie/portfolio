5c52defa4b1c7a6d38aa99478c2850f68fd11fc8
eesomie <oceanbateman19@gmail.com>
2025-02-05 18:01:05 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,218 @@
+fetch("assets.json")
+.then(response=>response.json())
+.then(data=>{
+    const section = document.getElementById("shelf")
+    data.forEach(item=>{
+        console.log(item);
+        const border = document.createElement("a");
+        border.href="view.html?img="+item;
+        border.classList.add("border");
+        const imgFile = "Assets/Art/"+item;
+        const newImg = document.createElement("img");
+        newImg.setAttribute("src", imgFile);
+        newImg.setAttribute("alt", imgFile);
+        border.appendChild(newImg);
+        section.appendChild(border);
+    })
+});
+
+
+
+fetch("characterConcepts.json")
+.then(response=>response.json())
+.then(data=>{
+    console.log(data);
+    const section = document.getElementById("characters")
+
+    var index = 0;
+
+
+    const leftBorder = document.createElement("div");
+    leftBorder.classList.add("border", "left");
+
+    const mainBorder = document.createElement("a");
+    mainBorder.classList.add("border", "focus");
+    
+
+    const rightBorder = document.createElement("div");
+    rightBorder.classList.add("border", "right");
+
+    function setImages(index){
+
+
+        if(index==0){
+        var leftImgFile = "Assets/Art/"+data[data.length-1];
+        }
+        else{
+            var leftImgFile = "Assets/Art/"+data[index-1];
+        }
+        const leftImg = document.createElement("img");
+        leftImg.setAttribute("src", leftImgFile);
+        leftImg.setAttribute("alt", leftImgFile);
+        leftBorder.appendChild(leftImg);
+
+        var mainImgFile = "Assets/Art/"+data[index];
+        const mainImg = document.createElement("img");
+        mainImg.setAttribute("src", mainImgFile);
+        mainImg.setAttribute("alt", mainImgFile);
+        mainBorder.appendChild(mainImg);
+
+        if(index==data.length-1){
+            var rightImgFile = "Assets/Art/"+data[0];
+        }
+        else{
+            var rightImgFile = "Assets/Art/"+data[index+1];
+        }
+        const rightImg = document.createElement("img");
+        rightImg.setAttribute("src", rightImgFile);
+        rightImg.setAttribute("alt", rightImgFile);
+        rightBorder.appendChild(rightImg);
+
+        mainBorder.href="view.html?img="+data[index]; 
+    }
+
+
+
+    const leftbutton = document.createElement("button");
+    leftbutton.setAttribute("type", "button");
+    leftbutton.addEventListener("click", ()=>{
+        if(index==0){
+            index = (data.length-1);
+            setImages(index);
+
+        }
+        else{
+            index -= 1;
+            setImages(index);
+
+        }
+        console.log("index = "+index);
+        console.log("img = "+data[index]);
+    });
+    leftbutton.appendChild(document.createTextNode("<<"));
+
+    const rightbutton = document.createElement("button");
+    rightbutton.setAttribute("type", "button");
+    rightbutton.addEventListener("click", ()=>{
+        if(index==(data.length-1)){
+            index = 0;
+            setImages(index);
+        }
+        else{
+            index += 1;
+            setImages(index);
+
+        }
+        console.log("index = "+index);
+        console.log("img = "+data[index]);
+    });
+     rightbutton.appendChild(document.createTextNode(">>"));
+
+
+    section.appendChild(leftbutton);
+    setImages(index);
+    section.appendChild(leftBorder);
+    section.appendChild(mainBorder);
+    section.appendChild(rightBorder);
+    section.appendChild(rightbutton);
+});
+
+
+fetch("environmentConcepts.json")
+.then(response=>response.json())
+.then(data=>{
+    console.log(data);
+    const section = document.getElementById("environments")
+
+    var index = 0;
+
+
+    const leftBorder = document.createElement("div");
+    leftBorder.classList.add("border", "left");
+
+    const mainBorder = document.createElement("a");
+    mainBorder.classList.add("border", "focus");
+    
+
+    const rightBorder = document.createElement("div");
+    rightBorder.classList.add("border", "right");
+
+    function setImages(index){
+
+
+        if(index==0){
+        var leftImgFile = "Assets/Art/"+data[data.length-1];
+        }
+        else{
+            var leftImgFile = "Assets/Art/"+data[index-1];
+        }
+        const leftImg = document.createElement("img");
+        leftImg.setAttribute("src", leftImgFile);
+        leftImg.setAttribute("alt", leftImgFile);
+        leftBorder.appendChild(leftImg);
+
+        var mainImgFile = "Assets/Art/"+data[index];
+        const mainImg = document.createElement("img");
+        mainImg.setAttribute("src", mainImgFile);
+        mainImg.setAttribute("alt", mainImgFile);
+        mainBorder.appendChild(mainImg);
+
+        if(index==data.length-1){
+            var rightImgFile = "Assets/Art/"+data[0];
+        }
+        else{
+            var rightImgFile = "Assets/Art/"+data[index+1];
+        }
+        const rightImg = document.createElement("img");
+        rightImg.setAttribute("src", rightImgFile);
+        rightImg.setAttribute("alt", rightImgFile);
+        rightBorder.appendChild(rightImg);
+
+        mainBorder.href="view.html?img="+data[index]; 
+    }
+
+
+
+    const leftbutton = document.createElement("button");
+    leftbutton.setAttribute("type", "button");
+    leftbutton.addEventListener("click", ()=>{
+        if(index==0){
+            index = (data.length-1);
+            setImages(index);
+
+        }
+        else{
+            index -= 1;
+            setImages(index);
+
+        }
+        console.log("index = "+index);
+        console.log("img = "+data[index]);
+    });
+    leftbutton.appendChild(document.createTextNode("<<"));
+
+    const rightbutton = document.createElement("button");
+    rightbutton.setAttribute("type", "button");
+    rightbutton.addEventListener("click", ()=>{
+        if(index==(data.length-1)){
+            index = 0;
+            setImages(index);
+        }
+        else{
+            index += 1;
+            setImages(index);
+
+        }
+        console.log("index = "+index);
+        console.log("img = "+data[index]);
+    });
+     rightbutton.appendChild(document.createTextNode(">>"));
+
+
+    section.appendChild(leftbutton);
+    setImages(index);
+    section.appendChild(leftBorder);
+    section.appendChild(mainBorder);
+    section.appendChild(rightBorder);
+    section.appendChild(rightbutton);
+});
---
+++ view.js
@@ -0,0 +1,15 @@
+const params = new URLSearchParams(window.location.search);
+const imageName = params.get('img');
+console.log(imageName);
+
+if (imageName) {
+    const section = document.getElementById("image");
+    const img = document.createElement("img");
+    const border = document.createElement("div")
+    border.classList.add("FSborder")
+    img.src = "/Assets/Art/"+imageName;
+    border.appendChild(img)
+    section.appendChild(border);
+} else {
+    document.body.innerHTML = '<p>No image specified.</p>';
+}

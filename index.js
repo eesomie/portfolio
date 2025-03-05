93d3e68bf72565c4da825b36cfc0f81cb07de88f
JakeDay06 <Jwday04@gmail.com>
2025-03-05 17:37:59 +0000

    hopefully made it so images in carousel are loaded with the page?

--- index.js
+++ index.js
@@ -13,218 +13,117 @@ fetch("assets.json")
         newImg.setAttribute("alt", imgFile);
         border.appendChild(newImg);
         section.appendChild(border);
-    })
+    });
 });
 
+function createCarousel(jsonFile, parent){
+
+    fetch(jsonFile)
+    .then(response=>response.json())
+    .then(data=>{
+        console.log(data);
+        const section = document.getElementById(parent)
+
+        var index = 0;
 
 
-fetch("characterConcepts.json")
-.then(response=>response.json())
-.then(data=>{
-    console.log(data);
-    const section = document.getElementById("characters")
+        const leftBorder = document.createElement("div");
+        leftBorder.classList.add("border", "left");
 
-    var index = 0;
+        const mainBorder = document.createElement("a");
+        mainBorder.classList.add("border", "focus");
+        
+
+        const rightBorder = document.createElement("div");
+        rightBorder.classList.add("border", "right");
 
 
-    const leftBorder = document.createElement("div");
-    leftBorder.classList.add("border", "left");
+        function setImages(index){
 
-    const mainBorder = document.createElement("a");
-    mainBorder.classList.add("border", "focus");
+            imgArray=[];
+            data.forEach((item)=>{
+                creatNewImg = document.createElement("img");
+                creatNewImg.setAttribute("src", "Assets/Thumbnails/"+item);
+                creatNewImg.setAttribute("alt", "Assets/Thumbnails/"+item);
+                imgArray.push(creatNewImg);
+            });
     
 
-    const rightBorder = document.createElement("div");
-    rightBorder.classList.add("border", "right");
+            if(index==0){
+                var leftImg = imgArray[imgArray.length-1];
+            }
+            else{
+                var leftImg = imgArray[index-1];
+            }
+            leftBorder.appendChild(leftImg);
 
-    function setImages(index){
+            var mainImg = imgArray[index];
+            mainBorder.appendChild(mainImg);
 
+            if(index==data.length-1){
+                var rightImg = imgArray[0];
+            }
+            else{
+                var rightImg = imgArray[index+1];
+            }
+            rightBorder.appendChild(rightImg);
 
-        if(index==0){
-        var leftImgFile = "Assets/Thumbnails/"+data[data.length-1];
+            mainBorder.href="view.html?img="+data[index]; 
         }
-        else{
-            var leftImgFile = "Assets/Thumbnails/"+data[index-1];
-        }
-        const leftImg = document.createElement("img");
-        leftImg.setAttribute("src", leftImgFile);
-        leftImg.setAttribute("alt", leftImgFile);
-        leftBorder.appendChild(leftImg);
-
-        var mainImgFile = "Assets/Thumbnails/"+data[index];
-        const mainImg = document.createElement("img");
-        mainImg.setAttribute("src", mainImgFile);
-        mainImg.setAttribute("alt", mainImgFile);
-        mainBorder.appendChild(mainImg);
-
-        if(index==data.length-1){
-            var rightImgFile = "Assets/Thumbnails/"+data[0];
-        }
-        else{
-            var rightImgFile = "Assets/Thumbnails/"+data[index+1];
-        }
-        const rightImg = document.createElement("img");
-        rightImg.setAttribute("src", rightImgFile);
-        rightImg.setAttribute("alt", rightImgFile);
-        rightBorder.appendChild(rightImg);
-
-        mainBorder.href="view.html?img="+data[index]; 
-    }
 
 
 
-    const leftbutton = document.createElement("button");
-    leftbutton.setAttribute("type", "button");
-    leftbutton.addEventListener("click", ()=>{
-        if(index==0){
-            index = (data.length-1);
-            setImages(index);
+        const leftbutton = document.createElement("button");
+        leftbutton.setAttribute("type", "button");
+        leftbutton.addEventListener("click", ()=>{
+            if(index==0){
+                index = (data.length-1);
+                setImages(index);
 
-        }
-        else{
-            index -= 1;
-            setImages(index);
+            }
+            else{
+                index -= 1;
+                setImages(index);
 
-        }
-        console.log("index = "+index);
-        console.log("img = "+data[index]);
+            }
+            console.log("index = "+index);
+            console.log("img = "+data[index]);
+        });
+        const leftSpan = document.createElement("span");
+        leftSpan.innerText = "♥︎";
+        leftbutton.appendChild(leftSpan);
+        leftbutton.classList.add("left");
+
+        const rightbutton = document.createElement("button");
+        rightbutton.setAttribute("type", "button");
+        rightbutton.addEventListener("click", ()=>{
+            if(index==(data.length-1)){
+                index = 0;
+                setImages(index);
+            }
+            else{
+                index += 1;
+                setImages(index);
+
+            }
+            console.log("index = "+index);
+            console.log("img = "+data[index]);
+        });
+        const rightSpan = document.createElement("span");
+        rightSpan.innerText = "♥︎";
+        rightbutton.appendChild(rightSpan);
+        rightbutton.classList.add("right");
+
+
+        section.appendChild(leftbutton);
+        setImages(index);
+        section.appendChild(leftBorder);
+        section.appendChild(mainBorder);
+        section.appendChild(rightBorder);
+        section.appendChild(rightbutton);
     });
-    const leftSpan = document.createElement("span");
-    leftSpan.innerText = "♥︎";
-    leftbutton.appendChild(leftSpan);
-    leftbutton.classList.add("left");
-
-    const rightbutton = document.createElement("button");
-    rightbutton.setAttribute("type", "button");
-    rightbutton.addEventListener("click", ()=>{
-        if(index==(data.length-1)){
-            index = 0;
-            setImages(index);
-        }
-        else{
-            index += 1;
-            setImages(index);
-
-        }
-        console.log("index = "+index);
-        console.log("img = "+data[index]);
-    });
-    const rightSpan = document.createElement("span");
-    rightSpan.innerText = "♥︎";
-    rightbutton.appendChild(rightSpan);
-     rightbutton.classList.add("right");
+};
 
 
-    section.appendChild(leftbutton);
-    setImages(index);
-    section.appendChild(leftBorder);
-    section.appendChild(mainBorder);
-    section.appendChild(rightBorder);
-    section.appendChild(rightbutton);
-});
-
-
-fetch("environmentConcepts.json")
-.then(response=>response.json())
-.then(data=>{
-    console.log(data);
-    const section = document.getElementById("environments")
-
-    var index = 0;
-
-
-    const leftBorder = document.createElement("div");
-    leftBorder.classList.add("border", "left");
-
-    const mainBorder = document.createElement("a");
-    mainBorder.classList.add("border", "focus");
-    
-
-    const rightBorder = document.createElement("div");
-    rightBorder.classList.add("border", "right");
-
-    function setImages(index){
-
-
-        if(index==0){
-        var leftImgFile = "Assets/Thumbnails/"+data[data.length-1];
-        }
-        else{
-            var leftImgFile = "Assets/Thumbnails/"+data[index-1];
-        }
-        const leftImg = document.createElement("img");
-        leftImg.setAttribute("src", leftImgFile);
-        leftImg.setAttribute("alt", leftImgFile);
-        leftBorder.appendChild(leftImg);
-
-        var mainImgFile = "Assets/Thumbnails/"+data[index];
-        const mainImg = document.createElement("img");
-        mainImg.setAttribute("src", mainImgFile);
-        mainImg.setAttribute("alt", mainImgFile);
-        mainBorder.appendChild(mainImg);
-
-        if(index==data.length-1){
-            var rightImgFile = "Assets/Thumbnails/"+data[0];
-        }
-        else{
-            var rightImgFile = "Assets/Thumbnails/"+data[index+1];
-        }
-        const rightImg = document.createElement("img");
-        rightImg.setAttribute("src", rightImgFile);
-        rightImg.setAttribute("alt", rightImgFile);
-        rightBorder.appendChild(rightImg);
-
-        mainBorder.href="view.html?img="+data[index]; 
-    }
-
-
-
-    const leftbutton = document.createElement("button");
-    leftbutton.setAttribute("type", "button");
-    leftbutton.addEventListener("click", ()=>{
-        if(index==0){
-            index = (data.length-1);
-            setImages(index);
-
-        }
-        else{
-            index -= 1;
-            setImages(index);
-
-        }
-        console.log("index = "+index);
-        console.log("img = "+data[index]);
-    });
-    const leftSpan = document.createElement("span");
-    leftSpan.innerText = "♥︎";
-    leftbutton.appendChild(leftSpan);
-    leftbutton.classList.add("left");
-
-    const rightbutton = document.createElement("button");
-    rightbutton.setAttribute("type", "button");
-    rightbutton.addEventListener("click", ()=>{
-        if(index==(data.length-1)){
-            index = 0;
-            setImages(index);
-        }
-        else{
-            index += 1;
-            setImages(index);
-
-        }
-        console.log("index = "+index);
-        console.log("img = "+data[index]);
-    });
-    const rightSpan = document.createElement("span");
-    rightSpan.innerText = "♥︎";
-    rightbutton.appendChild(rightSpan);
-     rightbutton.classList.add("right");
-
-
-    section.appendChild(leftbutton);
-    setImages(index);
-    section.appendChild(leftBorder);
-    section.appendChild(mainBorder);
-    section.appendChild(rightBorder);
-    section.appendChild(rightbutton);
-});
+createCarousel("characterConcepts.json", "characters");
+createCarousel("environmentConcepts.json", "environments");
